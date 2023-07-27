7d3e6abeba5d7f3d64136593529731ff232baf7d
Yasir Ali <yasir.ali@unitedsol.com>
2023-07-27 14:07:34 +0500

    Change database to Postgree

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('POSTGRES_URL'),
+            'host' => env('POSTGRES_HOST', ''),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('POSTGRES_DATABASE', 'forge'),
+            'username' => env('POSTGRES_USER', 'forge'),
+            'password' => env('POSTGRES_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
